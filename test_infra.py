141a3d5f748e0bc196eefe666a0d3e60447b0047
Christoph Kober <Christoph.Kober@software-craftsmen.at>
2022-06-20 15:37:08 +0200

    add app test

--- test_infra.py
+++ test_infra.py
@@ -1,3 +1,7 @@
 def test_docker_is_installed(host):
     docker = host.package("docker-ce")
     assert docker.is_installed
+
+def test_app_is_running(host)
+    app = host.docker("my-hero-app")
+    assert app.is_running
