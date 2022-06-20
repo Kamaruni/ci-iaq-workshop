cee033192f1c21a39c0efb7bb33ca0f69fe953ea
Christoph Kober <Christoph.Kober@software-craftsmen.at>
2022-06-20 15:42:51 +0200

    fix python syntax error

--- test_infra.py
+++ test_infra.py
@@ -2,6 +2,6 @@ def test_docker_is_installed(host):
     docker = host.package("docker-ce")
     assert docker.is_installed
 
-def test_app_is_running(host)
+def test_app_is_running(host):
     app = host.docker("my-hero-app")
     assert app.is_running
